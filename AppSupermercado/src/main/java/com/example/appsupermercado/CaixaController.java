c2f8e782668a844dfd59923841e8ccaced38bf56
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-01 10:03:18 -0300

    implementação funcionalidade caixa

--- AppSupermercado/src/main/java/com/example/appsupermercado/CaixaController.java
+++ AppSupermercado/src/main/java/com/example/appsupermercado/CaixaController.java
@@ -5,6 +5,24 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CaixaController {
+    @FXML
+    private TextField valorProdutoTextField;
 
+    @FXML
+    private TextField quantidadeTextField;
+
+    @FXML
+    private Label totalLabel;
+
+    @FXML
+    protected void calcularTotal() {
+        try {
+            double valorProduto = Double.parseDouble(valorProdutoTextField.getText());
+            int quantidade = Integer.parseInt(quantidadeTextField.getText());
+            double total = valorProduto * quantidade;
+            totalLabel.setText(String.format("%.2f", total));
+        } catch (NumberFormatException e) {
+            totalLabel.setText("Erro de entrada");
+        }
     }
-
+}
