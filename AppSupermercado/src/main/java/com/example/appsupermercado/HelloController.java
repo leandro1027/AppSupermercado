190ad4721369888fe0794df1f2c5b39378e2f1fe
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-11-18 00:48:54 -0300

    .

--- AppSupermercado/src/main/java/com/example/appsupermercado/HelloController.java
+++ AppSupermercado/src/main/java/com/example/appsupermercado/HelloController.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloController {
+
     @FXML
     protected void abrirCaixa() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("caixa-view.fxml"));
